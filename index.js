c10d366ebbe9f872bfeafd4e6017990ce555b105
rchaer53 <tayoshizawa29@gmail.com>
2019-03-13 17:36:25 +0900

    add dot file(ex. .gitignore)

--- index.js
+++ index.js
@@ -1,15 +1,43 @@
+const path = require('path')
 const fs = require('fs')
 const glob = require("glob")
+const archiver = require("archiver");
 
 const pattern = "./*"
+const output = fs.createWriteStream(path.join(__dirname, '../example.zip'))
 
+const archive = archiver('zip', {
+  zlib: { level: 9 }
+});
 const result = glob.sync(pattern, {
+  dot: true,
   ignore: './node_modules'
 })
 
-console.log(result)
+output.on('close', () => {
+  console.log(archive.pointer() + ' total bytes');
+  console.log('archiver has been finalized and the output file descriptor has closed.');
+});
 
-result.forEach((target) => {
-  const hoge = fs.statSync(target)
-  console.log(hoge.isDirectory())
+output.on('end', () => {
+  console.log('Data has been drained');
+});
+archive.pipe(output);
+
+result.forEach((targetPath) => {
+  const actualPath = path.resolve(targetPath)
+  if (fs.statSync(targetPath).isDirectory()) {
+    archive.directory(targetPath)
+  } else {
+    archive.file(actualPath, {
+      name: path.basename(targetPath)
+    })
+  }
+})
+
+archive.finalize();
+
+
+glob.sync(pattern, {
+  ignore: './node_modules'
 })
