7ba9dc01367ae477dd9a8c79dfa7dbc4bb208286
rchaser53 <tayoshizawa29@gmail.com>
2019-03-13 23:37:43 +0900

    try to unzip by unzip-stream

--- server.js
+++ server.js
@@ -1,60 +1,23 @@
-const path = require('path')
+const fs = require('fs')
+
 const express =  require('express')
 const formidableMiddleware =  require('express-formidable')
-
-const fs = require('fs')
+const unzip = require('unzip-stream')
 
 const app = express()
 app.use(formidableMiddleware())
 
-// const unzip = require('unzip')
-const unzip = require('unzip-stream')
-const fstream = require('fstream')
-
 app.post('/', async (req, res) => {
   try {
-    console.log(req.files.file)
-    const readStream = fs.createReadStream(req.files.file.path);
-    const writeStream = fs.createWriteStream('testste.zip')
-
-    readStream
-      .pipe(writeStream)
-    // const writeStream = fstream.Writer({
-    //   path: 'hogeDir',
-    //   type: 'Directory'
-    // });
-     
-    // readStream
-    //   .pipe(unzip.Parse())
-    //   .pipe(writeStream)
-    //   .on('close', (err) => {
-    //     if (err) {
-    //       console.error(err)
-    //       return
-    //     } else {
-          
-    //       console.log('safdsa')
-    //     }
-    //   })
-    //   .pipe(unzip.Parse())
-    //   .on('entry', (entry) => {
-    //     var fileName = entry.path;
-    //     var type = entry.type; // 'Directory' or 'File'
-    //     var size = entry.size;
-    //     if (fileName === "this IS the file I'm looking for") {
-    //       entry.pipe(fs.createWriteStream('output/path'));
-    //     } else {
-    //       entry.autodrain();
-    //     }
-    //   });
-
+    fs.createReadStream(req.files.file.path)
+      .pipe(unzip.Extract({ path: 'hongyaDir' }))
   } catch (err) {
     console.error(err)
   }
   
-  res.send('nya-n')
+  res.send('')
 })
 
 const server = app.listen(3000, () => {
-  console.log('nay-n')
+  console.log('run server')
 })
